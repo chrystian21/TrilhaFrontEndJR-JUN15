4cd728dc9f67e731d21e9a0ad64b45a3eda94bc5
Chrystian Carlos <Chrystiancarlosdasilva@gmail.com>
2024-07-10 16:55:20 -0300

    UPDATE: construção das últimas sessões da página, começo do código para deixar o site responsivo e nova função em js que esconde a nav bar.

---
+++ script.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector(".nav")
+
+//função que determina a velocidade do carrossel
+$('.carousel').carousel({
+    interval: 3000
+}) 
+
+//essa função verifica se a página esta rolando para baixo e esconde a nav bar
+
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (currentScroll > lastScrollTop) {
+        nav.classList.add("hidden")
+        
+    } else {
+        nav.classList.remove("hidden")
+    }
+    
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; 
+}, false);
+
